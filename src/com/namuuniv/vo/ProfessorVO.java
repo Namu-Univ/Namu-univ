d5adb08671b1271bb28702f3cc2be4ad26895abf
zzichu <jeesu1223@gmail.com>
2024-04-23 17:45:13 +0900

    add StudentVO, ProfessorVO, StaffVO

---
+++ src/com/namuuniv/vo/ProfessorVO.java
@@ -0,0 +1,71 @@
+package com.namuuniv.vo;
+
+import java.sql.Date;
+
+public class ProfessorVO {
+	
+	private int id;
+	private String name;
+	private String birthDate;
+	private String gender;
+	private String address;
+	private String tel;
+	private Date hireDate;
+	private int deptId;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public Date getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	
+	@Override
+	public String toString() {
+		return "ProfessorVO [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", gender=" + gender
+				+ ", address=" + address + ", tel=" + tel + ", hireDate=" + hireDate + ", deptId=" + deptId + "]";
+	}
+
+}
---
+++ src/com/namuuniv/vo/StaffVO.java
@@ -0,0 +1,71 @@
+package com.namuuniv.vo;
+
+import java.sql.Date;
+
+public class StaffVO {
+	
+	private int id;
+	private String name;
+	private String birthDate;
+	private String gender;
+	private String address;
+	private String tel;
+	private Date hireDate;
+	private String dept;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public Date getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	
+	@Override
+	public String toString() {
+		return "StaffVO [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", gender=" + gender + ", address="
+				+ address + ", tel=" + tel + ", hireDate=" + hireDate + ", dept=" + dept + "]";
+	}
+
+}
